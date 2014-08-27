352c1c1b1f573b4665ea3840c4d0b26b32259523
bu2 <bu2github@gmail.com>
2014-08-27 19:03:04 +0200

    Add a stupid benchmark and Rack::Test tests.

---
+++ test.rb
@@ -0,0 +1,249 @@
+ENV['RACK_ENV'] = 'test'
+
+require './backend'
+require 'test/unit'
+require 'rack/test'
+require 'json'
+
+
+
+$bob_auth_hash = { 
+  "provider"=>"developer",
+  "uid"=>"bob@yankee.com",
+  "info"=>{"name"=>"Bob",
+    "email"=>"bob@yankee.com"},
+  "credentials"=>{}, "extra"=>{}
+}
+
+$john_auth_hash = { 
+  "provider"=>"developer",
+  "uid"=>"john@doe.com",
+  "info"=>{"name"=>"John",
+    "email"=>"john@doe.com"},
+  "credentials"=>{}, "extra"=>{}
+}
+
+# OmniAuth.config.mock_auth[:developer] = OmniAuth::AuthHash.new(bob_auth_hash)
+OmniAuth.config.test_mode = true
+
+
+
+class BackendTest < Test::Unit::TestCase
+
+  # Test Helpers
+
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def authenticate_as_bob
+    OmniAuth.config.add_mock :developer, $bob_auth_hash
+    post('/auth/developer/callback', params: {} )
+  end
+
+  def authenticate_as_john
+    OmniAuth.config.add_mock :developer, $john_auth_hash
+    post('/auth/developer/callback', params: {} )
+  end
+
+  def homer
+    { 'firstname' => 'Homer', 'lastname' => 'Simpson', 'birthdate' => '1953-05-12' }
+  end
+
+  def homer_patch
+    { 'firstname' => '|-|0/\/\3R', 'lastname' => '$1/\/\P50|\|', 'mode' => 'L337 5P34|<' }
+  end
+
+  def marge
+    { 'firstname' => 'Marge', 'lastname' => 'Simpson', 'birthdate' => '1959-06-29' }
+  end
+
+  def add_homer
+    post('/api/people', homer.to_json, { 'Content-Type' => 'application/json' })
+    JSON.parse(last_response.body)['id']
+  end
+
+
+  
+  # Hello test
+
+  def test_it_says_hello_world
+    get '/'
+
+    assert last_response.ok?
+    assert_equal 'Hello World!', last_response.body
+  end
+
+
+
+  # Functional tests
+
+  def test_unauthenticated_api_access_forbidden
+    get '/api/people'
+    follow_redirect!
+
+    assert_equal 403, last_response.status
+    assert_equal 'forbidden', JSON.parse(last_response.body)['status']
+  end
+
+
+
+  def test_authentication_success
+    post('/auth/developer/callback', params: { name: 'Bob', email: 'bob@yankee.com' })
+
+    assert last_response.ok?
+    assert_equal 'ok', JSON.parse(last_response.body)['status']
+  end
+
+
+
+  def test_authenticated_api_access_success
+    authenticate_as_bob
+    get '/api/people'
+
+    assert last_response.ok?
+  end
+
+
+
+  def test_adding_one_person
+    authenticate_as_bob
+    id = add_homer
+
+    assert last_response.ok?
+    assert !id.nil?
+  end
+
+
+
+  def test_retrieving_one_person
+    authenticate_as_bob
+    id = add_homer
+    get "/api/people/#{id}"
+
+    assert last_response.ok?
+    result = JSON.parse(last_response.body)
+    result.delete('id')
+    result.delete('owner')
+    assert_equal homer, result
+  end
+
+
+
+  def test_replacing_homer_by_marge
+    authenticate_as_bob
+    id = add_homer
+    put("/api/people/#{id}", marge.to_json, { 'Content-Type' => 'application/json' })
+
+    assert last_response.ok?
+    assert_equal id, JSON.parse(last_response.body)['id']
+
+    get "/api/people/#{id}"
+
+    assert last_response.ok?
+    result = JSON.parse(last_response.body)
+    result.delete('id')
+    result.delete('owner')
+    assert_equal marge, result
+  end
+
+
+
+  def test_mutating_homer
+    authenticate_as_bob
+    id = add_homer
+    patch("/api/people/#{id}", homer_patch.to_json, { 'Content-Type' => 'application/json' })
+
+    assert last_response.ok?
+    assert_equal id, JSON.parse(last_response.body)['id']
+
+    get "/api/people/#{id}"
+
+    assert last_response.ok?
+    result = JSON.parse(last_response.body)
+    result.delete('id')
+    result.delete('owner')
+    assert_equal homer.merge(homer_patch), result
+  end
+
+
+
+  def test_deleting_homer
+    authenticate_as_bob
+    id = add_homer
+    delete "/api/people/#{id}"
+
+    assert last_response.ok?
+    assert_equal 'ok', JSON.parse(last_response.body)['status']
+
+    assert_raise NoMethodError, 'Homer still here!' do
+      get "/api/people/#{id}"
+    end
+  end
+
+
+
+  def test_fetching_people_list
+    authenticate_as_bob
+    get '/api/people'
+
+    assert last_response.ok?
+    assert_equal Array, JSON.parse(last_response.body).class
+  end
+
+
+
+  def test_finding_people
+    authenticate_as_bob
+    post('/api/people/find', { 'birthdate' => { '$lt' => '1955-01-01' } }.to_json, { 'Content-Type' => 'application/json' })
+
+    assert last_response.ok?
+    result = JSON.parse(last_response.body)
+    assert_equal Array, result.class
+    result.each do |person|
+      assert person['birthdate'] <= '1955-01-01'
+    end
+  end
+
+
+
+  def test_bob_access_his_data_only
+    authenticate_as_bob
+
+    get '/api/people'
+
+    assert last_response.ok?
+    result = JSON.parse(last_response.body)
+    assert_equal Array, result.class
+    result.each do |person|
+      assert_equal $bob_auth_hash['uid'], person['owner']
+    end
+  end
+
+  
+
+  def test_john_cant_access_bob_data
+    authenticate_as_bob
+    id = add_homer
+
+    authenticate_as_john
+    
+    assert_raise NoMethodError do
+      get "/api/people/#{id}"
+    end
+
+    post('/api/people/find', { owner: $bob_auth_hash['uid'] }.to_json, { 'Content-Type' => 'application/json' })
+
+    assert last_response.ok?
+    result = JSON.parse(last_response.body)
+    assert_equal Array, result.class
+    result.each do |person|
+      assert person['owner'] != $bob_auth_hash['uid']
+    end
+  end
+
+end
+
+
