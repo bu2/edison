d66e1da472a1f561cec33f766ac6ff8b669bfb72
bu2 <bu2github@gmail.com>
2014-10-04 00:47:07 +0200

    Plug MongoHQ addon for heroku.

--- backend.rb
+++ backend.rb
@@ -4,11 +4,19 @@ Bundler.require
 require 'sinatra/reloader' if development?
 
 require 'set'
+require 'uri'
 
 
 
-$mongo = Mongo::MongoClient.new
-$db = $mongo['app']
+def get_connection
+  return @db_connection if @db_connection
+  db = URI.parse(ENV['MONGOHQ_URL'])
+  db_name = db.path.gsub(/^\//, '')
+  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+  @db_connection
+end
+$db = get_connection
 
 $reserved_keys = [ '_id', '_owner', '_tags' ]
 
