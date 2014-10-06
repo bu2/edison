fc00289762a5d0c7115a5fe280157ab5cb2274a8
bu2 <bu2github@gmail.com>
2014-10-06 14:37:23 +0200

    Fix Travis CI setup.

--- before_install.sh
+++ before_install.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-# Ruby 2.1, Rubygems & Bundler
-sudo apt-get update
-sudo apt-get install rvm
-rvm use 2.1
-rvm install 2.1
-gem install bundler
-
 # MongoDB
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-rvm use 2.1
-# rvm install 2.1
 gem install bundler
 bundle install
 # ./mongo.sh
