17d3871317c51085fe2a1b18c6bcbb371d6046e3
bu2 <bu2github@gmail.com>
2014-10-06 14:25:01 +0200

    Fix Travis CI setup.

--- before_install.sh
+++ before_install.sh
@@ -12,5 +12,5 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-sudo service mongod stop
-sudo mv -fv /etc/init/mongod.conf /etc/init/mongod.conf.override
+# sudo service mongod stop
+# sudo mv -fv /etc/init/mongod.conf /etc/init/mongod.conf.override
--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+rvm use 2.1
+rvm install 2.1
+gem install bundler
 bundle install
-./mongo.sh
+# ./mongo.sh
